824990b4bdd0cdacb18ad62d8a1053f8ad883c69
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-05-20 15:13:53 +1000

    done with sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(userName, userEmail) { 
+    
+    const addUserData = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    }),
+    };
+    
+
+return fetch("http://localhost:3000/users", addUserData)
+   .then((res) => res.json())
+   .then((user) => {
+   const userID = user.id;
+   document.querySelector("body").append(userID)
+   })
+  .catch(function (error) {
+    const errorMessage = "FetchError: request to http://localhost:3000/users failed, reason: Unauthorized Access"
+    document.querySelector("body").append(errorMessage)
+  });
+
+};
+   
+   
+
+
+
+
+
+
+
